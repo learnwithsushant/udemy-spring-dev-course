fdd7417005b4a552fead243568c961b4d3258129
Sushant Wadjikar <wadjikarsushant@gmail.com>
2024-05-04 13:06:31 +0530

    first commit

---
+++ src/test/java/com/learn/springboot/UdemySpringBootCourse/UdemySpringBootCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springboot.UdemySpringBootCourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemySpringBootCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
